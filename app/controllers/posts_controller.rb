37d0fa67a01449f5bf948a418dbd3f8234451622
kensuke suzuki <35k.0101@gmail.com>
2020-09-29 19:19:57 +0900

    投稿機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,50 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "ブログを作成しました！"
+    else
+      render :new
+    end
+  end
+
+  def confirm
+    @blog = current_user.blogs.build(blog_params)
+    render :new if @blog.invalid?
+  end
+
+  def show
+    @post = current_user.posts.build(post_params)
+  end
+
+  def edit
+    @post = current_user.posts.build(post_params)
+  end
+
+  def update
+    @post = current_user.posts.build(post_params)
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice:"ブログを削除しました！"
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
